d0b0207e530c25f81643075679d9a112261206e3
ianhutson <57458226+ianhutson@users.noreply.github.com>
2023-12-29 19:02:34 -0500

    refactor

--- run.mjs
+++ run.mjs
@@ -7,118 +7,22 @@ const traktClientId = process.env.TRAKTCLIENTID;
 const traktClientSecret = process.env.TRAKTCLIENTSECRET;
 const tmdbApiKey = process.env.TMDBAPIKEY;
 const traktRefreshToken = process.env.TRAKTREFRESHTOKEN;
-const letterboxdUsername = process.env.LETTERBOXDUSERNAME
+const letterboxdUsername = process.env.LETTERBOXDUSERNAME;
 let newAccessToken;
 let newRefreshToken;
 
-const fetchWatchlistPage = async (page) => {
-  try {
-    const response = await fetch(
-      `https://letterboxd.com/${letterboxdUsername}/watchlist/page/${page}/`
-    );
-    const watchlistPage = await response.text();
-    return watchlistPage;
-  } catch (error) {
-    console.error("Error fetching watchlist page:", error);
-    throw error;
-  }
-};
-
-const fetchAllWatchlistPages = async (url) => {
-  const response = await fetch(url);
-  const watchlistPage = await response.text();
-  const $ = cheerio.load(watchlistPage);
-  const lastPage = parseInt($("li.paginate-page").last().text(), 10);
-  const allMovieTitles = [];
-  for (let currentPage = 1; currentPage <= lastPage; currentPage++) {
-    const pageHtml = await fetchWatchlistPage(currentPage);
-    const movieTitles = parseWatchlistPage(pageHtml);
-    allMovieTitles.push(...movieTitles);
-  }
-  return allMovieTitles;
-};
-
-const parseWatchlistPage = (pageHtml) => {
-  const $ = cheerio.load(pageHtml);
-  const movieTitles = [];
-  $(".poster-list .film-poster").each((index, element) => {
-    const titleSlug = $(element).attr("data-film-slug");
-    movieTitles.push(titleSlug);
-  });
-  return movieTitles;
-};
-
-const fetchMovieDetailsFromTMDb = async (movieTitle) => {
-  try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${tmdbApiKey}&query=${encodeURIComponent(
-        movieTitle
-      )}`
-    );
-    const data = await response.json();
-    if (data.results && data.results.length > 0) {
-      const movie = data.results[0];
-      return {
-        title: movie.title,
-        year: movie.release_date
-          ? new Date(movie.release_date).getFullYear()
-          : null,
-        ids: {
-          tmdb: movie.id,
-        },
-      };
-    }
-    return null;
-  } catch (error) {
-    console.error("Error fetching movie details from TMDb:", error);
-    console.log(data.status)
-    throw error;
-  }
-};
-
-const fetchTraktMovieDetails = async (movieTitle) => {
-  try {
-    const response = await fetch(
-      `https://api.trakt.tv/search/movie?query=${encodeURIComponent(
-        movieTitle
-      )}`,
-      {
-        timeout: 75000,
-        headers: {
-          "Content-Type": "application/json",
-          "trakt-api-version": "2",
-          "trakt-api-key": traktClientId,
-        },
-      }
-    );
-    const data = await response.json();
-    if (data && data.length > 0) {
-      const movie = data[0].movie;
-      return {
-        ids: {
-          trakt: movie.ids.trakt,
-          imdb: movie.ids.imdb,
-          slug: movie.ids.slug,
-        },
-      };
-    }
-    return null;
-  } catch (error) {
-    console.error("Error fetching movie details from Trakt:", error);
-    let retryCount = 0;
-    if (10 > retryCount) {
-      console.log(`Retrying request (Attempt ${retryCount + 1})...`);
-      retryCount++;
-      setTimeout(() => {
-        console.log("After pause");
-      }, 1000);
-      return fetchTraktMovieDetails(movieTitle);
-    }
-    throw error;
-  }
-};
-
-const addToTrakt = async (movieTitles) => {
+async function exportToTrakt() {
+  const movieTitles = [
+    ...(await fetchAndParseAllWatchlistPages(
+      "https://letterboxd.com/ayygux/watchlist/"
+    )),
+    ...(await fetchAndParseAllWatchlistPages(
+      "https://letterboxd.com/yanhut/watchlist/"
+    )),
+    ...(await fetchAndParseAllWatchlistPages(
+      "https://letterboxd.com/ayygux/list/alyssas-2023-criterion-challenge/"
+    )),
+  ];
   const newTraktToken = await getAccessTokenWithRefresh();
   const traktApiUrl = "https://api.trakt.tv/sync/watchlist";
   const headers = {
@@ -128,10 +32,6 @@ const addToTrakt = async (movieTitles) => {
     Authorization: `Bearer ${newTraktToken}`,
   };
   const movies = [];
-  console.log('beginning search for movie details for:')
-  for (const movieTitle of movieTitles){
-    console.log(movieTitle.replace('-', ' '))
-  }
   for (const movieTitle of movieTitles) {
     const tmdbMovieDetails = await fetchMovieDetailsFromTMDb(movieTitle);
     const traktMovieDetails = await fetchTraktMovieDetails(movieTitle);
@@ -148,8 +48,6 @@ const addToTrakt = async (movieTitles) => {
       });
     }
   }
-  console.log("Attempting to add the following movies-");
-  console.log(movies);
   const requestBody = {
     movies: movies,
   };
@@ -158,76 +56,107 @@ const addToTrakt = async (movieTitles) => {
     headers,
     body: JSON.stringify(requestBody),
   });
-  if (!response.ok) {
-    console.log("Response:" + (await response.text()));
-    throw new Error(
-      `Error adding movies with batch ${
-        moviesBatches.indexOf(moviesBatch) + 1
-      } out of ${moviesBatches.length}! Status: ${response.status}`
-    );
-  }
-};
+  console.log("Movies successfully added to Trakt watchlist");
+}
 
-async function exportToTrakt() {
-  try {
-    let movies = []
-    for (const movie of await fetchAllWatchlistPages("https://letterboxd.com/ayygux/watchlist/")){
-      movies.push(movie)
-    }
-    for (const movie of await fetchAllWatchlistPages("https://letterboxd.com/yanhut/watchlist/")){
-      movies.push(movie)
-    }
-    for (const movie of await fetchAllWatchlistPages("https://letterboxd.com/ayygux/list/alyssas-2023-criterion-challenge/")){
-      movies.push(movie)
-    }
-    await addToTrakt(movies);
-    console.log("Movies added to Trakt watchlist");
-  } catch (error) {
-    console.error("An error occurred:", error);
+async function fetchAndParseAllWatchlistPages(url) {
+  const response = await fetch(url);
+  const watchlistPage = await response.text();
+  const $ = cheerio.load(watchlistPage);
+  const lastPage = parseInt($("li.paginate-page").last().text(), 10);
+  const allMovieTitles = [];
+  for (let currentPage = 1; currentPage <= lastPage; currentPage++) {
+    const pageHtmlResponse = await fetch(
+      `https://letterboxd.com/${letterboxdUsername}/watchlist/page/${currentPage}/`
+    );
+    const pageHtml = await pageHtmlResponse.text();
+    const $ = cheerio.load(pageHtml);
+    const movieTitles = [];
+    $(".poster-list .film-poster").each((index, element) => {
+      const titleSlug = $(element).attr("data-film-slug");
+      movieTitles.push(titleSlug);
+    });
+    allMovieTitles.push(...movieTitles);
   }
-  console.log(`Old refresh token: ${traktRefreshToken}`);
-  console.log(`Current access token: ${newAccessToken}`);
-  console.log(`Current refresh token: ${newRefreshToken}`);
+  return allMovieTitles;
+}
+
+async function fetchMovieDetailsFromTMDb(movieTitle) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${tmdbApiKey}&query=${encodeURIComponent(
+      movieTitle
+    )}`
+  );
+  const data = await response.json();
+  if (data.results && data.results.length > 0) {
+    const movie = data.results[0];
+    return {
+      title: movie.title,
+      year: movie.release_date
+        ? new Date(movie.release_date).getFullYear()
+        : null,
+      ids: {
+        tmdb: movie.id,
+      },
+    };
+  }
+  return null;
+}
+
+async function fetchTraktMovieDetails(movieTitle) {
+  const response = await fetch(
+    `https://api.trakt.tv/search/movie?query=${encodeURIComponent(movieTitle)}`,
+    {
+      timeout: 75000,
+      headers: {
+        "Content-Type": "application/json",
+        "trakt-api-version": "2",
+        "trakt-api-key": traktClientId,
+      },
+    }
+  );
+  const data = await response.json();
+  if (data && data.length > 0) {
+    const movie = data[0].movie;
+    return {
+      ids: {
+        trakt: movie.ids.trakt,
+        imdb: movie.ids.imdb,
+        slug: movie.ids.slug,
+      },
+    };
+  }
+  return null;
 }
 
 async function getAccessTokenWithRefresh() {
   const traktApiUrl = "https://api.trakt.tv/oauth/token";
-  try {
-    const response = await fetch(traktApiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        refresh_token: traktRefreshToken,
-        client_id: traktClientId,
-        client_secret: traktClientSecret,
-        redirect_uri: "https://google.com",
-        grant_type: "refresh_token",
-      }),
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const responseData = await response.json();
-    newAccessToken = responseData.access_token;
-    newRefreshToken = responseData.refresh_token;
-    await updateVariableGroupVariable("TRAKTACCESSTOKEN", newAccessToken);
-    await updateVariableGroupVariable("TRAKTREFRESHTOKEN", newRefreshToken);
-    console.log("New Access Token:", newAccessToken);
-    console.log("New Refresh Token:", newRefreshToken);
-    return newAccessToken;
-  } catch (error) {
-    console.error("Error getting access token:", error.message);
-    throw error;
-  }
+  const response = await fetch(traktApiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      refresh_token: traktRefreshToken,
+      client_id: traktClientId,
+      client_secret: traktClientSecret,
+      redirect_uri: "https://google.com",
+      grant_type: "refresh_token",
+    }),
+  });
+  const responseData = await response.json();
+  newAccessToken = await responseData.access_token;
+  newRefreshToken = await responseData.refresh_token;
+  console.log(`Old refresh token: ${traktRefreshToken}`);
+  await updateVariableGroupVariable("TRAKTACCESSTOKEN", newAccessToken);
+  await updateVariableGroupVariable("TRAKTREFRESHTOKEN", newRefreshToken);
+  return newAccessToken;
 }
 
-export async function updateVariableGroupVariable(variableName, variableValue) {
+async function updateVariableGroupVariable(variableName, variableValue) {
   const organization = "yanhutson";
   const project = "letterdboxd-to-trakt";
   const variableGroupId = "1";
-  const maxRetries = 10; // Set the maximum number of retries
   const personalAccessToken = process.env.AZUREACCESSTOKEN;
   const url = `https://dev.azure.com/${organization}/${project}/_apis/distributedtask/variablegroups/${variableGroupId}?api-version=6.0-preview.2`;
   const headers = {
@@ -236,31 +165,17 @@ export async function updateVariableGroupVariable(variableName, variableValue) {
       "base64"
     )}`,
   };
-  let retries = 0;
-  while (retries < maxRetries) {
-    try {
-      const response = await fetch(url, { method: "GET", headers });
-      const responseData = await response.json();
-      responseData.variables[variableName].value = variableValue;
-      await fetch(url, {
-        method: "PUT",
-        headers,
-        body: JSON.stringify(responseData),
-      });
-      console.log(`Variable '${variableName}' updated successfully.`);
-      return; // Break out of the loop if successful
-    } catch (error) {
-      console.error(
-        `Error (Attempt ${retries + 1}/${maxRetries}):`,
-        error.message
-      );
-      retries++;
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-    }
-  }
-  console.error(
-    `Failed to update variable '${variableName}' after ${maxRetries} attempts.`
-  );
+  const response = await fetch(url, { method: "GET", headers });
+  const responseData = await response.json();
+  responseData.variables[variableName].value = variableValue;
+  await fetch(url, {
+    method: "PUT",
+    headers,
+    body: JSON.stringify(responseData),
+  });
+  console.log(`Variable '${variableName}' updated successfully.`);
+
+  console.log(`Current ${variableName}: ${variableValue}`);
 }
 
 exportToTrakt();
